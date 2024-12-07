leonissenbaum/BetterDiscordStuff

Added ChannelTabs Context Menus to threads
Previously, threads had no context menus. This meant, notably, that you could not favorite them.

Added context menus to them, allowing you to favorite them, as well as open them in a new tab.

Plugins/ChannelTabs/ChannelTabs.plugin.js
@@ -252,6 +252,30 @@ module.exports = (() => {
 				}]);
 			};
 
+			function CreateThreadChannelContextMenuChildren(instance, props)
+			{
+				return ContextMenu.buildMenuChildren([{
+					type: "group",
+					items: [
+						{
+							type: "submenu",
+							label: "ChannelTabs",
+							items: instance.mergeItems([
+								{
+									label: "Open in new tab",
+									action: ()=>TopBarRef.current && TopBarRef.current.saveChannel(props.channel.guild_id, props.channel.id, "#" + props.channel.name, getCurrentIconUrl(`/channels/${props.channel.guild_id}/${props.channel.parent_id}`) || "")
+								}],
+								[{
+									label: "Save channel as bookmark",
+									action: ()=>TopBarRef.current && TopBarRef.current.addToFavs("#" + props.channel.name, getCurrentIconUrl(`/channels/${props.channel.guild_id}/${props.channel.parent_id}`) || "", `/channels/${props.channel.guild_id}/${props.channel.id}`, props.channel.id)
+								}]
+							)
+						}
+					]
+				}]);
+			};
+
+
 			function CreateDMContextMenuChildren(instance, props)
 			{
 				return ContextMenu.buildMenuChildren([{
@@ -3380,6 +3404,11 @@ module.exports = (() => {
 						ContextMenu.patch("channel-context", (returnValue, props) => {
 							if(!this.settings.showTabBar && !this.settings.showFavBar) return;
 							returnValue.props.children.push(CreateTextChannelContextMenuChildren(this, props));
+						}),
+
+						ContextMenu.patch("thread-context", (returnValue, props) => {
+							if(!this.settings.showTabBar && !this.settings.showFavBar) return;
+							returnValue.props.children.push(CreateThreadChannelContextMenuChildren(this, props));
 						}),
 
 						ContextMenu.patch("user-context", (returnValue, props) => {

taker99/BetterDiscordStufftaker

Plugins/ChannelTabs/ChannelTabs.plugin.js
@@ -253,6 +253,30 @@ module.exports = (() => {
 				}]);
 			};
 
+			function CreateThreadChannelContextMenuChildren(instance, props)
+			{
+				return ContextMenu.buildMenuChildren([{
+					type: "group",
+					items: [
+						{
+							type: "submenu",
+							label: "ChannelTabs",
+							items: instance.mergeItems([
+								{
+									label: "Open in new tab",
+									action: ()=>TopBarRef.current && TopBarRef.current.saveChannel(props.channel.guild_id, props.channel.id, "#" + props.channel.name, getCurrentIconUrl(`/channels/${props.channel.guild_id}/${props.channel.parent_id}`) || "")
+								}],
+								[{
+									label: "Save thread as bookmark",
+									action: ()=>TopBarRef.current && TopBarRef.current.addToFavs("#" + props.channel.name, getCurrentIconUrl(`/channels/${props.channel.guild_id}/${props.channel.parent_id}`) || "", `/channels/${props.channel.guild_id}/${props.channel.id}`, props.channel.id)
+								}]
+							)
+						}
+					]
+				}]);
+			};
+
+
 			function CreateDMContextMenuChildren(instance, props)
 			{
 				return ContextMenu.buildMenuChildren([{
@@ -3404,6 +3428,11 @@ module.exports = (() => {
 						ContextMenu.patch("channel-context", (returnValue, props) => {
 							if(!this.settings.showTabBar && !this.settings.showFavBar) return;
 							returnValue.props.children.push(CreateTextChannelContextMenuChildren(this, props));
+						}),
+
+						ContextMenu.patch("thread-context", (returnValue, props) => {
+							if(!this.settings.showTabBar && !this.settings.showFavBar) return;
+							returnValue.props.children.push(CreateThreadChannelContextMenuChildren(this, props));
 						}),
 
 						ContextMenu.patch("user-context", (returnValue, props) => {