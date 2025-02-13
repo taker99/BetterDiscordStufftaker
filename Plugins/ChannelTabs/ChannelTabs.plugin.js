d0ddd5031b94e67d39dfedcfc958de635005fb80
samfundev <samdinner@hotmail.com>
2025-02-13 10:39:58 -0500

    Bump version

--- Plugins/ChannelTabs/ChannelTabs.plugin.js
+++ Plugins/ChannelTabs/ChannelTabs.plugin.js
@@ -2,7 +2,7 @@
  * @name ChannelTabs
  * @author samfundev, l0c4lh057, CarJem Generations
  * @description Allows you to have multiple tabs and bookmark channels.
- * @version 2.6.16
+ * @version 2.6.17
  * @authorId 76052829285916672
  * @donate https://github.com/sponsors/samfundev
  * @source https://github.com/samfundev/BetterDiscordStuff/blob/master/src/ChannelTabs/index.jsx
