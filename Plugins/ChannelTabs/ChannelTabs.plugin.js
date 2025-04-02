1543dd21dc1d2a0b8fbd0323679756532ccd59c3
samfundev <samdinner@hotmail.com>
2025-04-02 01:14:10 -0400

    Bump version

--- Plugins/ChannelTabs/ChannelTabs.plugin.js
+++ Plugins/ChannelTabs/ChannelTabs.plugin.js
@@ -2,7 +2,7 @@
  * @name ChannelTabs
  * @author samfundev, l0c4lh057, CarJem Generations
  * @description Allows you to have multiple tabs and bookmark channels.
- * @version 2.6.18
+ * @version 2.7.0
  * @authorId 76052829285916672
  * @donate https://github.com/sponsors/samfundev
  * @source https://github.com/samfundev/BetterDiscordStuff/blob/master/src/ChannelTabs/index.jsx
@@ -31,6 +31,10 @@
 
 @else@*/
 const CHANGES = {
+	"2.7.0": {
+		fixed: ['Fixed "Open in New Tab" not working with certain settings'],
+		improved: ["Combined the tabs with Discord's new titlebar"],
+	},
 	"2.6.18": {
 		fixed: ["Fixed server icon not working"],
 		added: ["Open new tab using CTRL+T"],
